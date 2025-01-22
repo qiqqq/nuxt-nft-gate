363597c08f31544b4095caa8cbdcdbf80041ef11
Tomasz Kikowski <tomaszkikowski@gmail.com>
2025-01-22 12:24:02 +0100

    fix: resolve eslint errors

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,8 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  myModule: {},
   devtools: { enabled: true },
+  nftGate: {
+    nftAddress: '0x3727aC93ED1FF0472eC91619CfaA011F76A5BAAe',
+  },
+  compatibilityDate: '2025-01-22',
 })
--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,9 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver, addComponent, addImportsDir } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  nftAddress?: string | string[]
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
